e5bbb27add8da72b639bf95add604084e53669b7
MutisyaVuta <nicholas.mutisya77@gmail.com>
2024-03-29 13:23:08 +0300

    Finish draft of new code-along

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const birthdayList = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(array, event = "event"){
+  let newArray = [];
+  for (let i = 0; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return newArray
+  }
+console.log(writeCards(birthdayList, "birthday"));
+
+const countDown = (positiveInteger) => {
+    let i = positiveInteger;
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+
+}
+countDown(10);
